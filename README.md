caa73044df08a94aea330fe65800e1f60a63302b
Endeyr <aaron.thompson42594@gmail.com>
2022-10-06 10:48:29 -0400

    add axios functionality,

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Followed Part 3 of freeCodeCamp.org React Javascript Framework Course found here: https://youtu.be/u6gSSpfsoOQ.
 
-CSS powered by Bootstrap 5.
-
-Finnhub Api for stock information.
+- CSS powered by Bootstrap 5.
+- Finnhub Api for stock information.
+- Axios for API fetching
 
 ## Concepts Used
 
--- src/components/Stocklist.js
+++ src/components/Stocklist.js
@@ -1,6 +1,41 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import finnHub from '../apis/finnHub'
 
 export const Stocklist = () => {
+	const [stock, setStock] = useState()
 	const [watchList, setWatchList] = useState(['GOOGL', 'MSFT', 'AMZN'])
+
+	useEffect(() => {
+		let isMounted = true
+		const fetchData = async () => {
+			const responses = []
+			try {
+				const responses = await Promise.all(
+					watchList.map((stock) => {
+						return finnHub.get('/quote', {
+							params: {
+								symbol: stock,
+							},
+						})
+					})
+				)
+
+				const data = responses.map((response) => {
+					return {
+						data: response.data,
+						symbol: response.config.params.symbol,
+					}
+				})
+				console.log(data)
+				if (isMounted) {
+					setStock(data)
+				}
+			} catch (err) {}
+		}
+		fetchData()
+
+		return () => (isMounted = false)
+	}, [])
+
 	return <div>Stock list</div>
 }
